cdad3b9bc34c537bf55328835d2798c6338e5614
mmall <sun951cake@163.com>
2018-06-20 10:09:12 +0800

    切换到分支进行测试

--- src/main/java/com.git.test/GitTest.java
+++ src/main/java/com.git.test/GitTest.java
@@ -3,5 +3,7 @@ package com.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git...");
+
+        System.out.println("切换到分支测试。。。");
     }
 }
